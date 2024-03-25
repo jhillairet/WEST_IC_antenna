16fbfc97725df6ae34c1095342500af388b5a02e
Julien Hillairet <julien.hillairet@gmail.com>
2024-03-25 23:02:49 +0100

    matching tests

--- west_ic_antenna/test/test_antenna.py
+++ west_ic_antenna/test/test_antenna.py
@@ -106,9 +106,9 @@ def test_capa_model_bridge_antenna_ports(antenna_default_arg):
     # ANSYS Equivalent circuit with complex bridge and antenna ports' Z0
     ANSYS_model = "ANSYS_benchmarks/WEST_capacitor_equivalent_circuit_bridge_antenna_ports.s2p"
     cap_ANSYS = rf.Network(os.path.join(TEST_DIR, ANSYS_model))
-    
+
     assert cap == cap_ANSYS
-    
+
 
 def test_capa_model_bridge_antenna_ports_renorm50ohm(antenna_default_arg):
     """
@@ -129,21 +129,18 @@ def test_capa_model_bridge_antenna_ports_renorm50ohm(antenna_default_arg):
 def test_capa_model_connection(antenna_default_arg):
     """
     Benchmark dummy circuit model of a antenna-capa-bridge vs ANSYS Circuit.
-    
+
     In ANSYS, a single capacitor electrical circuit is connected to one bridge
     output and to an antenne front face input (using HFSS model).
     Bridge input is connected to input port and all other ports are shorted.
-    
+
     Benchmark is performed when the Touchtone file is exported without and with
     50 Ohm renormalization.
 
     """
-    # NB: importing S-matrix in ANSYS Circuit as a N-port component 
-    # requires the user to know in advance the port char impedance
-    # ant = antenna_default_arg
     ant = antenna_default_arg
     cap = ant.capa(C=50)
-    
+
     # Creating the dummy circuit using scikit-rf
     cap.name = 'capa'
     port = rf.Circuit.Port(frequency=ant.frequency, name='port1', z0=ant.bridge.z0[:,0].real)
@@ -157,20 +154,77 @@ def test_capa_model_connection(antenna_default_arg):
     ]
     cir = rf.Circuit(cnx)
     ntw = cir.network
-    
+
     # ANSYS Model Export without renormalization
     ANSYS_model = "ANSYS_benchmarks/WEST_capacitor_test_connection.s1p"
     ANSYS_connection_test = rf.Network(os.path.join(TEST_DIR, ANSYS_model))
-    
-    assert ntw == ANSYS_connection_test   
-     
+
+    assert ntw == ANSYS_connection_test
+
     # ANSYS Model Export Renomalized to 50 Ohm
     ANSYS_model_50 = "ANSYS_benchmarks/WEST_capacitor_test_connection_renorm50ohm.s1p"
     ANSYS_connection_test_50 = rf.Network(os.path.join(TEST_DIR, ANSYS_model_50))
     ntw.renormalize(50)
-    
-    assert ntw == ANSYS_connection_test_50   
-    
+
+    assert ntw == ANSYS_connection_test_50
+
+# Various return value tests
+def test_antenna_circuit(antenna_default_arg):
+    """Test the internal circuit."""
+    ant = antenna_default_arg
+    assert isinstance(ant.circuit(), rf.Circuit)
+    assert isinstance(ant.circuit(Cs=[50,50,50,50]), rf.Circuit)
+
+    # TODO benchmark against ANSYS for a specific case of capa set
+
+def test_Cs(antenna_default_arg):
+    ant = antenna_default_arg
+    assert isinstance(ant.Cs, list)
+
+
+# Matching Tests (longer...)
+def test_match_one_side_left(antenna_default_arg):
+    # Test the return values of left side matching
+    Cs = antenna_default_arg.match_one_side(
+        f_match=55e6,
+        solution_number=1,
+        side="left",
+        z_match=29.89 + 0j
+        )
+    # reference values 25/03/2024
+    np.testing.assert_allclose(Cs, [51.44282751806046, 49.325275021316564, 150, 150], rtol=1e-5)
+
+def test_match_one_side_right(antenna_default_arg):
+    # Test the return values of right side matching
+    Cs = antenna_default_arg.match_one_side(
+        f_match=55e6,
+        solution_number=1,
+        side="right",
+        z_match=29.89 + 0j
+        )
+    # reference values 25/03/2024
+    np.testing.assert_allclose(Cs, [150, 150, 51.20960703555699, 49.489644871023714], rtol=1e-5)
+
+def test_match_both_sides_separately(antenna_default_arg):
+    # Test the return values of both sides matching (separately)
+    Cs = antenna_default_arg.match_both_sides_separately(
+        f_match=55e6,
+        solution_number=1,
+        z_match=[29.89 + 0j, 29.87 + 0j],
+        )
+    # reference values 25/03/2024
+    np.testing.assert_allclose(Cs, [51.44282792582701, 49.32527088864287, 51.20987339854108, 49.48946981138618], rtol=1e-1)
+
+def test_match_both_sides(antenna_default_arg):
+    # Test the return values of both sides matching
+    Cs = antenna_default_arg.match_both_sides(
+        f_match=55e6,
+        solution_number=1,
+        z_match=[29.89 + 0j, 29.87 + 0j],
+        )
+    # reference values 25/03/2024
+    np.testing.assert_allclose(Cs, [52.09695694, 50.08075395, 51.62129529, 50.25710599], rtol=1e-1)
+
 
 if __name__ == "__main__":
     pytest.main([__file__])
