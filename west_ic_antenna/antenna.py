a6c43693014ef1c519eb45f416479a2a4f2b2388
Julien Hillairet <julien.hillairet@gmail.com>
2025-01-03 18:25:42 +0100

    use Circuit.update_networks
    - add _capacitors
    - use Circuit.update_networks instead of rebuilding the full circuit
    
    before update: 324586.09ms for 20 vacuum matching iterations
    after update: 305841.10ms for 20 vacuum matching iterations
    so a gain of around 10% speed-up.

--- west_ic_antenna/antenna.py
+++ west_ic_antenna/antenna.py
@@ -241,7 +241,7 @@ class WestIcrhAntenna:
             Bridge side characteristic impedance in [Ohm].
             The default is bridge z0.
         z0_antenna : float, optional
-            Antenna side charactetistic impedance in [Ohm].
+            Antenna side characteristic impedance in [Ohm].
             The default is the antenna z0
         name : str, optional
             capacitor Network name. Default is 'capa'.
@@ -290,6 +290,39 @@ class WestIcrhAntenna:
 
         return capa
 
+    def _capacitors(self, Cs: NumberLike) -> List["rf.Network"]:
+        """
+        Create the four capacitor Networks
+
+        Parameters
+        ----------
+        Cs : list or array or None
+            antenna 4 capacitances [C1, C2, C3, C4] in [pF].
+            Default is None (use internal Cs).
+
+        Returns
+        -------
+        C1_ntw, C2_ntw, C3_ntw, C4_ntw : rf.Network
+            capacitor networks
+
+        """
+        C1, C2, C3, C4 = Cs
+        # left side
+        C1_ntw = self.capa(
+            C1, z0_bridge=self.bridge_left.z0[0][1], z0_antenna=self.antenna.z0[0][0], name="C1"
+        )
+        C2_ntw = self.capa(
+            C2, z0_bridge=self.bridge_left.z0[0][2], z0_antenna=self.antenna.z0[0][2], name="C2"
+        )
+        # right side
+        C3_ntw = self.capa(
+            C3, z0_bridge=self.bridge_right.z0[0][1], z0_antenna=self.antenna.z0[0][1], name="C3"
+        )
+        C4_ntw = self.capa(
+            C4, z0_bridge=self.bridge_right.z0[0][2], z0_antenna=self.antenna.z0[0][3], name="C4"
+        )
+        return C1_ntw, C2_ntw, C3_ntw, C4_ntw
+
     def _antenna_circuit(self, Cs: NumberLike) -> "rf.Circuit":
         """
         Antenna scikit-rf Circuit.
@@ -305,21 +338,8 @@ class WestIcrhAntenna:
             Antenna Circuit
 
         """
-        C1, C2, C3, C4 = Cs
-        # left side
-        capa_C1 = self.capa(
-            C1, z0_bridge=self.bridge_left.z0[0][1], z0_antenna=self.antenna.z0[0][0], name="C1"
-        )
-        capa_C2 = self.capa(
-            C2, z0_bridge=self.bridge_left.z0[0][2], z0_antenna=self.antenna.z0[0][2], name="C2"
-        )
-        # right side
-        capa_C3 = self.capa(
-            C3, z0_bridge=self.bridge_right.z0[0][1], z0_antenna=self.antenna.z0[0][1], name="C3"
-        )
-        capa_C4 = self.capa(
-            C4, z0_bridge=self.bridge_right.z0[0][2], z0_antenna=self.antenna.z0[0][3], name="C4"
-        )
+        # capacitor Networks
+        capa_C1, capa_C2, capa_C3, capa_C4 = self._capacitors(Cs)
 
         # WARNING !
         # antenna port numbering convention does not follow capa and voltage :
@@ -389,7 +409,13 @@ class WestIcrhAntenna:
 
         """
         Cs = Cs or self.Cs
-        self._circuit = self._antenna_circuit(Cs)
+        if not hasattr(self, '_circuit'):
+            # Create full circuit
+            self._circuit = self._antenna_circuit(Cs)
+        else:
+            # Update only the capacitors Networks to speed up
+            C1_ntw, C2_ntw, capa_C3_ntw, C4_ntw = self._capacitors(Cs)
+            self._circuit = self._circuit.update_networks([C1_ntw, C2_ntw, capa_C3_ntw, C4_ntw])
         return self._circuit
 
     def _optim_fun_one_side(
